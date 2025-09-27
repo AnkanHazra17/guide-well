36608e4c9bc3ff5c0c2758a298476071d43e71fb
ankan17 <ankan.hazra1710@gmail.com>
2025-09-27 21:44:32 +0800

    feat: convex package

---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,15 @@
+import { mutation, query } from "./_generated/server";
+
+export const getMany = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("users").collect();
+  },
+});
+
+export const add = mutation({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.insert("users", { name: "Ankan" });
+  },
+});
